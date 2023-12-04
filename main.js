35f172220c1f6c724496b713eae1388bcd426081
elisamardiyani0 <141213160+elisamardiyani0@users.noreply.github.com>
2023-12-04 13:15:14 +0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+nama = "Nama: Elisa mardiyani"
+kelas ="Kelas: XI"
+no_absen = "No absen: 08"
+jurusan="Jurusan: PPLG"
+alamat="Alamat: Kp.Gunung salikur"
+
+document.write(nama + "<br>", kelas + "<br>", no_absen + "<br>", jurusan + "<br>", alamat)
